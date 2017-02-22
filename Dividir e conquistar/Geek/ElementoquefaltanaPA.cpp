affb96b8a55a7c7e82e578c3c86373236305cb14
tiagomariinhoo <tiago.marinho.797@gmail.com>
2017-02-22 09:46:29 -0300

    Geek

---
+++ Dividir e conquistar/Geek/ElementoquefaltanaPA.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+#define DEBUG cout << "AQUI" << endl;
+using namespace std;
+
+int main()
+{
+	int cases;
+		cin >> cases;
+			for(int m=0;m<cases;m++){
+				int n;
+					cin >> n;
+					vector <int> vec;
+
+						for(int i=0;i<n;i++){
+							int element;
+								cin >> element;
+								vec.push_back(element);
+						}
+
+						int razao = (vec[vec.size()-1]-vec[0])/((vec.size()));
+
+						for(int i=1;i<vec.size();i++){
+							if(vec[i]-vec[i-1]!=razao){
+								cout << vec[i]-razao << endl;
+							}
+						}
+
+			}
+
+	return 0;
+}
