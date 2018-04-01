bde3770fd48fe489e6b80e8c23fcef4be7e39733
Tiago Marinho <tiago.marinho.797@gmail.com>
2018-04-01 16:17:19 -0300

    Ops

--- Maratona/Sagaz/IceSkating(CF - DFS).cpp
+++ Maratona/Sagaz/IceSkating(CF - DFS).cpp
@@ -27,8 +27,39 @@ int ddx[] = {1, 0};
 int ddy[] = {1, 1};
 //\n
 
-int main(){
+int n;
+vector<ii> vec;
+int vis[1010];
 
+void dfs(int x){
+	vis[x] = 1;
+	for(int i=0;i<sz(vec);i++){
+		if(vis[i]) continue;
+		else {
+			if(vec[i].F == vec[x].F or vec[x].S == vec[i].S){
+				dfs(i);
+			}
+		}
+	}
+}
+
+int main(){
+	cin >> n;
+	for(int i=0;i<n;i++){
+		int a,b;
+		cin >> a >> b;
+		vec.pb({a,b});
+	}
+
+	int ans = 0;
+	for(int i=0;i<sz(vec);i++){
+		if(!vis[i]){
+			dfs(i);
+			ans++;
+		}
+	}
+
+	cout << ans - 1 << endl;
 
 	return 0;
 }
